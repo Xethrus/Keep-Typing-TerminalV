f99beb03107e9d819ba1820947bc34af6593761c
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-03-22 14:17:41 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,7 @@ void makeVector(std::ifstream &inFile){
               std::back_inserter(allWords));
 }
 void typeWord(std::vector<std::string> currentVector){
-    std::cout << "Press \"0\" To exit" << std::endl;
-    int n = 0;
-    while(!GetKeyState('0'){
-        std::cout << "Please Type The word \"" << currentVector[n] << ": ";
-        
-    }
+    //TODO lol make this do stuff
 }
 
 int main(){
