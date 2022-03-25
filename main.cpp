ed0498e0d5c74da6abcde5ec02bb9c401c64257e
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-03-25 07:06:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,45 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <random>
 #include <iterator>
 #include <string>    
 
-void makeVector(std::ifstream &inFile){
-    std::vector<std::string> allWords;
+void fillVector(std::ifstream &inFile, std::vector<std::string> &allWords){
     std::copy(std::istream_iterator<std::string>(inFile),
               std::istream_iterator<std::string>(),
               std::back_inserter(allWords));
 }
-void typeWord(std::vector<std::string> currentVector){
-    //TODO lol make this do stuff
+void shuffleVector(std::vector<std::string> &vectorToBeShuffled){
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(vectorToBeShuffled), std::end(vectorToBeShuffled), rng);
+}
+std::string getString(void){
+    std::string userString;
+    std::cout << "enter string" << std::endl;
+    std::cin >> userString;
+    return userString;
+}
+void typeWord(std::string inString, std::vector<std::string> vectorIndex,int counterIndex){
+    if(inString != vectorIndex[counterIndex]) {
+        std::cout << "incorrect spelling" << std::endl;
+    } else {
+        std::cout << "correct" << std::endl;
+    }
+
+}
+void iterateAllWords(std::vector<std::string> &vectorToBeIterated){
+    int counterIndex = 0;
+    for (auto & element : vectorToBeIterated){
+        //TODO make this do the thing LOL (check if word typed)
+        counterIndex++;
+        typeWord(getString(),vectorToBeIterated,counterIndex);
+    }
+}
+void printVector(std::vector<std::string> vectorToBePrinted){
+    for (auto & element : vectorToBePrinted){
+        std::cout << element << std::endl;
+    }
 }
 
 int main(){
@@ -23,8 +51,16 @@ int main(){
         exit(1);
     }
     if(wordTextFile.is_open()) {
-        //std::cout << wordTextFile.rdbuf();
-        makeVector(wordTextFile);
+        std::cout << wordTextFile.rdbuf();
+        std::vector<std::string> allWords;
+        fillVector(wordTextFile, allWords);
+        
+        //testing things LOL
+        printVector(allWords);
+        shuffleVector(allWords); //not working for intended purpose
+        printVector(allWords);
+
+
     }
     return 0;
 };
